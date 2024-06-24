90f3f6e00416efd89b134f837afb13114fa90449
tian8280508 <751531219@qq.com>
2024-06-24 18:40:44 +0800

    finish a demo

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,34 @@
 #include <iostream>
 
 int main() {
-    TimberSaw::Options options;
-    return 0;
+  std::cout << "main begin" << std::endl;
+  // 设置数据库选项
+  TimberSaw::Options options;
+  std::cout << "main co 2" << std::endl;
+  options.create_if_missing = true;
+  // 数据库指针
+  TimberSaw::DB *db;
+  TimberSaw::Status status = TimberSaw::DB::Open(options, "testdb", &db);
+
+  if (!status.ok()) {
+    std::cerr << "Unable to open/create test database 'testdb'" << std::endl;
+    std::cerr << status.ToString() << std::endl;
+    return -1;
+  }
+
+  // 设置写选项
+  TimberSaw::WriteOptions write_options;
+
+  // 执行 Put 操作
+  status = db->Put(write_options, "key", "value");
+
+  if (status.ok()) {
+    std::cout << "Put operation successful" << std::endl;
+  } else {
+    std::cerr << "Put operation failed: " << status.ToString() << std::endl;
+  }
+
+  // 关闭数据库
+  delete db;
+  return 0;
 }
